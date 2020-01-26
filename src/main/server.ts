c06d110cce59170284a5939a1496bb0718caf8d3
juniordesenv <jr.miranda@outlook.com>
2020-01-26 12:45:01 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,13 @@
-import app from './config/app';
 
-app.listen(5050, () => {
-  console.log('Server running at http://localhost:5050');
-});
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongoHelper';
+
+import env from './config/env';
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+    app.listen(env.port, () => {
+      console.log(`Server running at http://localhost:${env.port}`);
+    });
+  })
+  .catch(console.error);
