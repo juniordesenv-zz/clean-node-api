8416e7dfcdba9ad3e477320ea36db80972a1b1c9
Junior Miranda <jr.miranda@outlook.com>
2020-05-30 08:25:47 -0300

    chore: enable debug with webstorm

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,13 @@
 import { Express, Router } from 'express';
 
-import fg from 'fast-glob';
+import { readdirSync } from 'fs';
 
 export default (app: Express): void => {
   const router = Router();
   app.use('/api', router);
-  fg.sync('**/src/main/routes/**Routes.ts').map(async (file) => {
-    (await import(`../../../${file}`)).default(router);
+  readdirSync(`${__dirname}/../routes`).map(async (file) => {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
+      (await import(`../routes/${file}`)).default(router);
+    }
   });
 };
