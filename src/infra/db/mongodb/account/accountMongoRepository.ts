aba6d9be5a8ef8edbbffeba425d42af3720e05fa
Junior Miranda <jr.miranda@outlook.com>
2020-06-01 18:21:38 -0300

    feat: ensure correct load account by loadByToken

--- src/infra/db/mongodb/account/accountMongoRepository.ts
+++ src/infra/db/mongodb/account/accountMongoRepository.ts
@@ -31,4 +31,13 @@ export class AccountMongoRepository implements AddAccountRepository,
       },
     });
   }
+
+  async loadByToken(token: string, role?: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({
+      accessToken: token,
+      role,
+    });
+    return account && MongoHelper.map(account);
+  }
 }
