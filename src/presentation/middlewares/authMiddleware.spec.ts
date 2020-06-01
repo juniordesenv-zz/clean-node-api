c6ec53b3560b5fc620a3b7ce4a042be135f7b4d5
Junior Miranda <jr.miranda@outlook.com>
2020-06-01 14:15:51 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct role

--- src/presentation/middlewares/authMiddleware.spec.ts
+++ src/presentation/middlewares/authMiddleware.spec.ts
@@ -32,9 +32,9 @@ interface SutTypes {
   loadAccountByTokenStub: LoadAccountByToken;
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (role?: string): SutTypes => {
   const loadAccountByTokenStub = makeLoadAccountByTokenStub();
-  const sut = new AuthMiddleware(loadAccountByTokenStub);
+  const sut = new AuthMiddleware(loadAccountByTokenStub, role);
   return {
     sut,
     loadAccountByTokenStub,
@@ -50,11 +50,12 @@ describe('Auth Middleware', () => {
 
 
   test('Should call LoadAccountByToken with correct accessToken', async () => {
-    const { loadAccountByTokenStub } = makeSut();
+    const role = 'any_role';
+    const { loadAccountByTokenStub } = makeSut(role);
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load');
-    const sut = new AuthMiddleware(loadAccountByTokenStub);
+    const sut = new AuthMiddleware(loadAccountByTokenStub, role);
     await sut.handle(makeFakeRequest());
-    expect(loadSpy).toHaveBeenCalledWith('any_token');
+    expect(loadSpy).toHaveBeenCalledWith('any_token', 'any_role');
   });
 
   test('Should return 403 if LoadAccountByToken returns null', async () => {
--- src/presentation/middlewares/authMiddleware.ts
+++ src/presentation/middlewares/authMiddleware.ts
@@ -6,6 +6,7 @@ import { LoadAccountByToken } from '~/domain/usecases/loadAccountByToken';
 export class AuthMiddleware implements Middleware {
   constructor(
     private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role: string,
   ) {
   }
 
@@ -13,7 +14,7 @@ export class AuthMiddleware implements Middleware {
     try {
       const accessToken = httpRequest.headers?.Authorization;
       if (accessToken) {
-        const account = await this.loadAccountByToken.load(accessToken);
+        const account = await this.loadAccountByToken.load(accessToken, this.role);
         if (account) {
           return ok({
             accountId: account.id,
