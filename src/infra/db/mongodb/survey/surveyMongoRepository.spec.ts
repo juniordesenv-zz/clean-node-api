5b64afc2e30cff1bbd2aab5345cd9ff6b0ff872d
Junior Miranda <jr.miranda@outlook.com>
2020-06-02 13:58:08 -0300

    feat: ensure SurveyMongoRepository load all suverys

--- src/infra/db/mongodb/survey/surveyMongoRepository.spec.ts
+++ src/infra/db/mongodb/survey/surveyMongoRepository.spec.ts
@@ -19,23 +19,58 @@ describe('Account Mongo Repository', () => {
 
   const makeSut = (): SurveyMongoRepository => new SurveyMongoRepository();
 
-  test('Should add a survey on success', async () => {
-    const sut = makeSut();
-    await sut.add({
-      question: 'any_question',
-      answers: [
-        {
-          image: 'any_image',
-          answer: 'any_answer',
-        },
-        {
-          answer: 'other_answer',
-        },
-      ],
-      createdAt: new Date(),
-    });
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
+      const sut = makeSut();
+      await sut.add({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer',
+          },
+          {
+            answer: 'other_answer',
+          },
+        ],
+        createdAt: new Date(),
+      });
 
-    const survey = await surveyCollection.findOne({ question: 'any_question' });
-    expect(survey).toBeTruthy();
+      const survey = await surveyCollection.findOne({ question: 'any_question' });
+      expect(survey).toBeTruthy();
+    });
+  });
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      const sut = makeSut();
+      await surveyCollection
+        .insertMany([
+          {
+            question: 'any_question',
+            answers: [
+              {
+                image: 'any_image',
+                answer: 'any_answer',
+              },
+            ],
+            createdAt: new Date(),
+          },
+          {
+            question: 'other_question',
+            answers: [
+              {
+                image: 'other_image',
+                answer: 'other_answer',
+              },
+            ],
+            createdAt: new Date(),
+          },
+        ]);
+      const surveys = await sut.loadAll();
+      expect(surveys.length).toBe(2);
+      expect(surveys[0].question).toEqual('any_question');
+      expect(surveys[1].question).toEqual('other_question');
+    });
   });
 });
--- src/infra/db/mongodb/survey/surveyMongoRepository.ts
+++ src/infra/db/mongodb/survey/surveyMongoRepository.ts
@@ -2,11 +2,18 @@ import { AddSurveyRepository } from '../../../../data/protocols/db/survey/addSur
 import { SurveyModel } from '../../../../domain/models';
 import { MongoHelper } from '../helpers/mongoHelper';
 import { AddSurveyModel } from '../../../../domain/usecases/addSurvey';
+import { LoadSurveysRepository } from '~/data/protocols/db/survey/loadSurveysRepository';
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add(surveyData: AddSurveyModel): Promise<SurveyModel> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
     const result = await surveyCollection.insertOne(surveyData);
     return MongoHelper.map(result.ops[0]);
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const surveys: SurveyModel[] = await surveyCollection.find().toArray();
+    return surveys;
   }
 }
